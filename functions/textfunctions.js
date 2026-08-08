997233668ff362ce202091d0741947f6180ba1ee
Enraa <enraaaery@gmail.com>
2026-08-07 19:48:05 -0700

    unmask behind locks

--- functions/textfunctions.js
+++ functions/textfunctions.js
@@ -4,6 +4,7 @@ const { getChastityBra } = require("./getters/chastity/getChastityBra.js");
 const { getCollar } = require("./getters/collar/getCollar.js");
 const { getUserTags } = require("./getters/config/getUserTags.js");
 const { getGag } = require("./getters/gag/getGag.js");
+const { getBaseHeadwear } = require("./getters/headwear/getBaseHeadwear.js");
 const { getHeadwear } = require("./getters/headwear/getHeadwear.js");
 const { getHeadwearRestrictions } = require("./getters/headwear/getHeadwearRestrictions.js");
 const { getHeavy } = require("./getters/heavy/getHeavy.js");
@@ -4557,9 +4558,33 @@ const texts_unheadwear = {
 		nomitten: {
 			self: {
 				single: {
-                    locked: [
+                    noaccess: [
                         `USER_TAG tries to put a finger underneath the VAR_C2 to get some leverage and remove it, but the locks keep it firmly secured to USER_THEIR head!`,
                         `Despite USER_THEIR best efforts, the VAR_C2 does not budge from USER_TAG's head without the key. USER_THEY_CAP should find who has it and ask for it back.`
+                    ],
+                    locked: [
+                        `USER_TAG carefully undoes the straps on the VAR_C2, gently pulling it off of USER_THEIR head!`,
+                        `USER_TAG runs USER_THEIR fingers carefully over the VAR_C2 and undoes it, letting it fall gently into USER_THEIR lap.`,
+                        `The VAR_C2 slips off slowly as the straps on it are unbuckled, freeing USER_TAG from it's effects!`,
+                        // Twice as likely on blindfolds. 
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
+                            },
+                            text: `USER_TAG blinks and squints as USER_THEIR eyes adjust to the light again after being in the darkness from the VAR_C2!`
+                        },
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
+                            },
+                            text: `USER_TAG blinks and squints as USER_THEIR eyes adjust to the light again after being in the darkness from the VAR_C2!`
+                        },
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockemote
+                            },
+                            text: `USER_TAG's face is no longer hidden as USER_THEY removeUSER_S the VAR_C2!`
+                        },
                     ],
 					worn: [
                         `USER_TAG carefully undoes the straps on the VAR_C2, gently pulling it off of USER_THEIR head!`,
@@ -4568,19 +4593,19 @@ const texts_unheadwear = {
                         // Twice as likely on blindfolds. 
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockinspect
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
                             },
                             text: `USER_TAG blinks and squints as USER_THEIR eyes adjust to the light again after being in the darkness from the VAR_C2!`
                         },
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockinspect
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
                             },
                             text: `USER_TAG blinks and squints as USER_THEIR eyes adjust to the light again after being in the darkness from the VAR_C2!`
                         },
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockemote
+                                return getBaseHeadwear[t.headwearchoice]?.blockemote
                             },
                             text: `USER_TAG's face is no longer hidden as USER_THEY removeUSER_S the VAR_C2!`
                         },
@@ -4596,29 +4621,52 @@ const texts_unheadwear = {
 			},
 			other: {
 				single: {
-                    locked: [
+                    noaccess: [
                         `USER_TAG tugs against the VAR_C2 on TARGET_TAG's head, but without some elaborate lockpicking tools, there's no hope of undoing the locks on it!`,
                         `Sadly, USER_TAG does not have the key to unlock the VAR_C2 on TARGET_TAG's head. A shame.`
                     ],
-					worn: [
+					locked: [
                         `USER_TAG runs USER_THEIR hands on TARGET_TAG's head, unclasping the straps to TARGET_THEIR VAR_C2 and taking it off!`,
                         `USER_TAG carefully slips the VAR_C2 off of TARGET_TAG's head, granting TARGET_THEM just a small semblance of freedom!`,
                         // Twice as likely on blindfolds. 
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockinspect
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
                             },
                             text: `TARGET_TAG blinks and squints as TARGET_THEIR eyes adjust to the light again after USER_TAG rescues USER_THEM from the darkness of the VAR_C2!`
                         },
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockinspect
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
                             },
                             text: `TARGET_TAG blinks and squints as TARGET_THEIR eyes adjust to the light again after USER_TAG rescues USER_THEM from the darkness of the VAR_C2!`
                         },
                         {
                             required: (t) => {
-                                return process.headtypes[t.headwearchoice]?.blockemote
+                                return getBaseHeadwear[t.headwearchoice]?.blockemote
+                            },
+                            text: `TARGET_TAG's face is no longer hidden as USER_TAG removes the VAR_C2!`
+                        },
+                    ],
+                    worn: [
+                        `USER_TAG runs USER_THEIR hands on TARGET_TAG's head, unclasping the straps to TARGET_THEIR VAR_C2 and taking it off!`,
+                        `USER_TAG carefully slips the VAR_C2 off of TARGET_TAG's head, granting TARGET_THEM just a small semblance of freedom!`,
+                        // Twice as likely on blindfolds. 
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
+                            },
+                            text: `TARGET_TAG blinks and squints as TARGET_THEIR eyes adjust to the light again after USER_TAG rescues USER_THEM from the darkness of the VAR_C2!`
+                        },
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockinspect
+                            },
+                            text: `TARGET_TAG blinks and squints as TARGET_THEIR eyes adjust to the light again after USER_TAG rescues USER_THEM from the darkness of the VAR_C2!`
+                        },
+                        {
+                            required: (t) => {
+                                return getBaseHeadwear[t.headwearchoice]?.blockemote
                             },
                             text: `TARGET_TAG's face is no longer hidden as USER_TAG removes the VAR_C2!`
                         },
