a5f7682680bd81e71183b76a15436fb3b9606499
Enraa <enraaaery@gmail.com>
2026-08-11 08:30:32 -0700

    NAME

--- functions/interactivefunctions.js
+++ functions/interactivefunctions.js
@@ -37,6 +37,8 @@ const { canAccessChastityBra } = require("./getters/chastity/canAccessChastityBr
 const { getChastityBraName } = require("./getters/chastity/getChastityBraName.js");
 const { getRecentChannel } = require("./getters/config/getRecentChannel.js");
 const { getItemTags } = require("./getters/config/getItemTags.js");
+const { getGagName } = require("./getters/gag/getGagName.js");
+const { getBaseHeavy } = require("./getters/heavy/getBaseHeavy.js");
 
 // Generates a consent button which the user will have to agree to.
 const consentMessage = (interaction, user) => {
@@ -712,7 +714,7 @@ async function handleExtremeRestraint(serverID, user, target, type, restraint) {
                 }
             })
         }
-		if (!hasOption || hasOption == "Enabled" || (hasOption == "PromptOthers" && user.id == target.id)) {
+		if (!hasOption || hasOption == "Enabled" || (hasOption == "PromptOthers")) {
 			res(true);
 			return;
 		} // Either it's Enabled, set to Prompt Others if on self, or it doesn't exist. Go away.
@@ -734,10 +736,10 @@ async function handleExtremeRestraint(serverID, user, target, type, restraint) {
 				restraintfullname = getHeavyName(restraint);
 				break;
 			case "gag":
-				restraintfullname = process.autocompletes.gag.find((f) => f.value == restraint)?.name;
+				restraintfullname = getGagName(restraint);
 				break;
             case "mask":
-				restraintfullname = process.autocompletes.headtypes.find((f) => f.value == restraint)?.name;
+				restraintfullname = getHeadwearName(restraint);
 				break;
 			default:
 				console.log(`Could not find a restraint by that type.`);
@@ -827,7 +829,22 @@ async function handleMajorRestraint(serverID, user, target, type, restraint) {
 				restraintfullname = getHeavyName(restraint);
                 prettytype = "Heavy Bondage"
                 emoji = `${process.emojis.armbinder}`;
-                limitationstext = `This will prevent you from using most commands in the bot, including **/unheavy** to free yourself!`
+                limitationstext = `Something weird happened with heavy tags, so please let the dev know including what item was attempted here!`
+                let htags = getBaseHeavy(restraint).heavytags;
+                if (htags.includes("container")) {
+                    limitationstext = `This will limit you to only being able to perform actions on yourself and others in the same container!`
+                }
+                if (htags.includes("legs")) {
+                    limitationstext = `This will limit you to only being able to apply or remove restraints from yourself in the bot!`
+                }
+                if (htags.includes("arms")) {
+                    limitationstext = `This will prevent you from using most commands in the bot, including **/unheavy** to free yourself!`
+                }
+                // If it's furniture, do we REALLY need to worry? 
+                if (htags.length == 0) {
+                    res(true);
+                    return
+                }
 				break;
 			case "chastity":
 				restraintfullname = getBaseChastity(restraint)?.name;
@@ -848,7 +865,7 @@ async function handleMajorRestraint(serverID, user, target, type, restraint) {
                 limitationstext = `This will prevent you from adding or removing gags with **/gag** or masks with **/mask** until someone else unmittens you!`
                 break;
             case "mask":
-                restraintfullname = getHeadwearName(serverID, undefined, restraint);
+                restraintfullname = getHeadwearName(restraint);
                 prettytype = "Mask"
                 emoji = `${process.emojis.gasmask}`;
                 limitationstext = `This may have a major effect on your speech or emoji, as well as blinding you in **/inspect**!`
